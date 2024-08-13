fdc6c56ae9a34f7ba9011d4048955e6965d7c4dd
Bogdan <bog_berdsk@mail.ru>
2024-08-13 22:35:31 +0700

    13.08

--- module_1_6.py
+++ module_1_6.py
@@ -3,19 +3,21 @@ print("*" * 30)
 print("Задание N°2")
 print("*" * 30)
 my_dict = {"Понедельник": 1, "Вторник": 2, "Среда": 3, "Четверг": 4, "Пятница": 5, "Суббота": 6, "Воскресенье": 7}
-print(my_dict)
+print("Словарь: ", my_dict)
 print()
 
-print(my_dict.get("Понедельник"))
-print(my_dict.get("Фамилия"))
+print("Значение ключа 'Понедельник': ", my_dict.get("Понедельник"))
+print("Значение ключа 'Фамилия': ", my_dict.get("Фамилия"))
 print()
 
 my_dict.update({"Monday": 1, "Tuesday": 2, "Wednesday": 3, "Thursday": 4, "Friday": 5, "Saturday": 6, "Sunday": 7})
-print(my_dict)
+print("Обновленный словарь: ", my_dict)
 print()
 
-my_dict.pop("Воскресенье")
-print(my_dict)
+x = my_dict.pop("Воскресенье")
+print("Вырезан item: ", my_dict)
+print()
+print("Тот самый вырезанный item: ", x)
 print()
 # ********************
 
@@ -25,16 +27,16 @@ print("Задание N°3")
 print("*" * 30)
 my_set = {1, 1, 2, 2, 1, 14, 20, 20, 5, 6, 2024, 8, 8, 1, 1, 5}
 
-print(my_set)
+print("Множество: ", my_set)
 print()
 
 my_set.update((9, 10, 19))         # добавление элементов
 
-print(my_set)
+print("Новое множество: ", my_set)
 print()
 
 my_set.remove(8)                    # удаление символа
-print(my_set)
+print("8 Удалено из множества", my_set)
 print()
 # ********************
 
